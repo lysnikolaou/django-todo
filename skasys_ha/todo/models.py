8626cc2842e1fcc52698669e19b95ecb89b2b859
nickchrysaf <49755456+nickchrysaf@users.noreply.github.com>
2019-05-11 15:54:22 +0200

    Model_View_Test (#2)
    Add model for Tasks and templates.

--- skasys_ha/todo/models.py
+++ skasys_ha/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    text = models.CharField(max_length=160)
+    date = models.DateField()
+    progress = models.IntegerField()
