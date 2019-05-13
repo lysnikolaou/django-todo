43387acc18872b4fb769e16e842b2ed3f02f6106
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-05-13 15:53:10 +0200

    Add, edit and delete functionality implementation

--- skasys_ha/todo/urls.py
+++ skasys_ha/todo/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('tasks/', views.get_tasks, name='get_tasks'),
+    path('tasks/add/', views.add_task, name='add_task'),
+    path('about/', views.about, name='about'),
+    path('tasks/<int:id>/edit', views.edit_task, name='edit_task'),
+    path('tasks/<int:id>/delete', views.delete_task, name='delete_task')
 ]
